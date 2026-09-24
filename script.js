1beae4383b598caaa1e0bb62430ec6e5d51a4c36
thiago860 <thiagoliam.rodriguez5@gmail.com>
2026-09-24 18:19:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -107,6 +107,17 @@ const productos = [
         imagen: "imagenes/soporte-joystick.jpeg",
         emoji: "🎮"
     }
+  },
+
+    {
+        id: 7,
+        nombre: "soporte para celular para el dia de la madre",
+        precio: 8000,
+        categoria: "soportes",
+        descripcion: "para el dia de la madres no te lo pierdas.",
+        imagen: "imagenes/soporte-feliz dia madre.jpeg",
+        emoji: "📱"
+    },
 
 ];
 
